8db7b47462371136a5387fdb26d2c57bbcaf5511
medhaviniputhalapattu <81733706+medhaviniputhalapattu@users.noreply.github.com>
2021-04-14 12:04:04 +0530

    first commit

---
+++ ONLINE_INDOOR_PLANTS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ONLINE_INDOOR_PLANTS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
